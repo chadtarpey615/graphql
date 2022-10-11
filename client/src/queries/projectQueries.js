4440eae529f1b9c1bccc55e5eb4ffee1af66d92b
Chad Tarpey <chadtarpey@gmail.com>
2022-10-11 07:45:26 -0500

    Created pages folder for react-router

--- client/src/queries/projectQueries.js
+++ client/src/queries/projectQueries.js
@@ -9,4 +9,22 @@ const GET_PROJECTS = gql`
         }
     }`
 
-    export {GET_PROJECTS};
+
+ const GET_PROJECT = gql`
+    query GetProject($id: ID!) {
+        project(id: $id) {
+            id
+            name
+            descritpion
+            status
+            client {
+                id
+                name
+                email
+                phone
+            }
+        }
+    }`
+   
+
+    export {GET_PROJECTS, GET_PROJECT};
